8e52d6c0fcdae57633d51b8fbe664d1cf668f4f5
jlbuild <juliabuildbot@gmail.com>
2026-03-06 08:03:54 +0000

    p7zip_jll build 17.8.0+0

--- src/p7zip_jll.jl
+++ src/p7zip_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("p7zip")
-JLLWrappers.@generate_main_file("p7zip", UUID("3f19e933-33d8-53b3-aaab-bd5110c3b7a0"))
+JLLWrappers.@generate_main_file("p7zip", Base.UUID("3f19e933-33d8-53b3-aaab-bd5110c3b7a0"))
 end  # module p7zip_jll
